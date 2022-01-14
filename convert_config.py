dcb50170c28bd4cbdf262b94c200d52aeb8c0287
paulgrammer <paulmugaya@live.com>
2022-01-14 21:32:01 +0300

    webhook http request lib

--- convert_config.py
+++ convert_config.py
@@ -1,6 +1,11 @@
+import os
 from collections import OrderedDict
 
+path = './data'
 
+if not os.path.exists(path): 
+  os.makedirs(path)
+  
 class Config:
   def __init__(self):
     self.__dict__ = OrderedDict([
